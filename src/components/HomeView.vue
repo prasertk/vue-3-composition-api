2087f7d3e15d0a9d927a9c2060f1e09249322822
prasertk <prasertk@gmail.com>
2022-11-16 10:55:51 +0700

    update homeview

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -5,20 +5,43 @@
         <button @click="counter--">Down</button>
 
         <h3>{{ person.name }}  {{person.age}}</h3>
-        <button>Update Person</button>
+        <button @click="updatePerson">Update Person</button>
+        <h3>{{brand}}</h3>
     </main>
 </template>
 
 <script setup lang="ts">
-    import { reactive, ref } from 'vue';
+    import { onMounted, reactive, ref } from 'vue';
     const counter = ref(0)
 
     const person = reactive({name: 'Prasert', age:40})
 
     const updatePerson =() => {
-        
+        person.name='Namo',
+        person.age=15
     }
 
+    onMounted(() => {
+        setTimeout(() => {
+            counter.value =500
+            }, 2000)       
+    })
+    
+</script>
+
+<script lang="ts">
+   export default {
+        data(){
+            return {
+                brand: "ITGenius"
+            }
+        },
+        mounted(){
+            setTimeout(() => {
+            this.brand ="Other brand"
+            }, 2000)
+        }
+   }
 </script>
 
 <style scoped>
