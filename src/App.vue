8b9728a6303c6c7c23db628db17fd19996aface5
prasertk <prasertk@gmail.com>
2022-11-16 10:54:27 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- <h1>{{title}}</h1>
+  <p>{{message}}</p>
+  <p>{{year}}</p> -->
+
+  <HomeView />
+</template>
+
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import HomeView from './components/HomeView.vue';
+
+  // const title =ref("Hello World !")
+  // const message =ref("Welcome to everybody")
+  // const year = ref(2000)
+
+  // setTimeout(() => {
+  //   title.value ="Hello Toe !"
+  // }, 2000);
+
+</script>
+
+<!-- <script lang="ts">
+import { defineComponent,ref,reactive } from 'vue' -->
+
+// export default defineComponent({
+  //   //Composition API
+  //   setup () {
+  //     //ref จะรองรับ premitive type string, number,boolean including object
+  //     // let title =ref("Hello World !")
+  //     // const message =ref("Welcome to everybody")
+  //     // const year = ref(2000)
+  //     // console.log(title)
+  //     // const data= ref({
+  //     //   title: "Hello World!",
+  //     //   message:"Welcome to everybody",
+  //     //   year:2000
+  //     // })
+  
+  //     //Reactive จะรองรับเฉพาะ Object เท่านั้น !!!
+  //     const data= reactive({
+  //       title: "Hello World!",
+  //       message:"Welcome to everybody",
+  //       year:2000
+  //     })
+  
+  
+  //     setTimeout(() => {
+  //       // title.value ="Hello Toe !"
+  //       // data.value.title ="Hello Toe !"
+  
+  //       data.title="Hello Toe !"
+  //     }, 2000);
+  
+  
+  //     // return {title,message,year}
+  //     return {data}
+  //   },
+  
+  //   //====Option API====
+  //   // data(){
+  //   //   return {
+  //   //     title: "Hello World!"
+  //   //   }
+  //   // },
+  //   // mounted(){
+  //   //   setTimeout(() => {
+  //   //     this.title ="Hello Toe !"
+  //   //   }, 2000);
+  //   // }
+  
+  // })
+  <!-- // </script> -->
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <button @click="counter++">Up</button>
+        <h3>{{ counter }}</h3>
+        <button @click="counter--">Down</button>
+
+        <h3>{{ person.name }}  {{person.age}}</h3>
+        <button>Update Person</button>
+    </main>
+</template>
+
+<script setup lang="ts">
+    import { reactive, ref } from 'vue';
+    const counter = ref(0)
+
+    const person = reactive({name: 'Prasert', age:40})
+
+    const updatePerson =() => {
+        
+    }
+
+</script>
+
+<style scoped>
+
+</style>
